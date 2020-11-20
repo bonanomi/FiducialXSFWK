86c427adaa614d96e112221ee9cfe9b5e9e03087
mbonanom <matteo.bonanomi@cern.ch>
2020-11-20 15:05:19 +0100

    Theoretical H xs and BR

--- env.sh
+++ env.sh
@@ -6,6 +6,5 @@ mkdir combine_files;
 mkdir datacards; cd datacards; mkdir datacard_2016; mkdir datacad_2017; mkdir datacard_2018;
 cd $base_path;
 mkdir plots;
-mkdir inputs;
 cd templates; mkdir 2016; mkdir 2017; mkdir 2018;
 cd $base_path;
