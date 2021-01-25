6a1e0db2e1dbbae8fd1232766751bfc3f80ba413
Alessandro Tarabini <alessandro.tarabini@gmail.com>
2021-01-25 16:19:24 +0100

    bkg kin discriminant skimmer

--- config/skimmer.sh
+++ config/skimmer.sh
@@ -6,10 +6,18 @@
 #    done
 #done
 
-for year in 2016 2017 2018
+#for year in 2016 2017 2018
+#do
+#    for process in ggH125 VBFH125 WplusH125 WminusH125 ZH125 ttH125 ZZTo4lext ggTo2e2mu_Contin_MCFM701 ggTo2e2tau_Contin_MCFM701 ggTo2mu2tau_Contin_MCFM701 ggTo4e_Contin_MCFM701 ggTo4mu_Contin_MCFM701 ggTo4tau_Contin_MCFM701
+#    do
+#        root -q -b skim_MC_tree.C"(\"$process\",\"$year\")"
+#    done
+#done
+
+for year in 2016
 do
-    for process in ggH125 VBFH125 WplusH125 WminusH125 ZH125 ttH125 ZZTo4lext ggTo2e2mu_Contin_MCFM701 ggTo2e2tau_Contin_MCFM701 ggTo2mu2tau_Contin_MCFM701 ggTo4e_Contin_MCFM701 ggTo4mu_Contin_MCFM701 ggTo4tau_Contin_MCFM701
+    for process in ZZTo4lext
     do
-        root -q -b skim_MC_tree.C"(\"$process\",\"$year\")"
+        root -q -b skim_MC_tree_Matteo.C"(\"$process\",\"$year\")"
     done
 done
